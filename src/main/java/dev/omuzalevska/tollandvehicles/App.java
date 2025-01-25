b2bed42a91d785dd90dddec2cc1de309d8ef285c
OMuzalevska <oksana.muzalevskaja@gmail.com>
2025-01-25 23:07:36 +0100

    change readme

--- src/main/java/dev/omuzalevska/tollandvehicles/App.java
+++ src/main/java/dev/omuzalevska/tollandvehicles/App.java
@@ -4,10 +4,9 @@ public class App {
     public static void main(String[] args) {
     TollStation station = new TollStation("Central Toll", "Kyiv");
 
-    // Adding vehicles
     station.processVehicle(new Car("ABC-123"));
     station.processVehicle(new Motorcycle("DEF-456"));
-    station.processVehicle(new Truck("GHI-789", 3)); // 3 axles
+    station.processVehicle(new Truck("GHI-789", 3)); 
 
     station.printReport();
     }
